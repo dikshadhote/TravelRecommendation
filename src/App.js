74bfa216a8ad4d1f6e9341fb7f68a1f9b691e497
dikshadhote <39937404+dikshadhote@users.noreply.github.com>
2021-01-21 23:19:23 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
 import "./styles.css";
-
+var color = "red";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>
+        Travel Recommendation <span>🏝️</span>{" "}
+      </h1>
+      <p>
+        Checkout my favorite places to visit in{" "}
+        <span style={{ color }}>India</span>.Select a place to get started
+      </p>
+      <div>
+        <button
+          style={{
+            padding: "0.2rem",
+            cursor: "pointer",
+            margin: "0.5rem",
+            width: "80px"
+          }}
+        >
+          Mumbai
+        </button>
+        <button
+          style={{
+            padding: "0.2rem",
+            cursor: "pointer",
+            margin: "0.5rem",
+            width: "80px"
+          }}
+        >
+          Pune
+        </button>
+        <button
+          style={{
+            padding: "0.2rem",
+            cursor: "pointer",
+            margin: "0.5rem",
+            width: "80px"
+          }}
+        >
+          Agra
+        </button>
+      </div>
     </div>
   );
 }
